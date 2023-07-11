f37746ba60ec75c23f72c15a499b515c2f9f7fd9
Ing. Luis Almazán <luis.14xbox@live.com.mx>
2023-07-10 20:09:01 -0600

    loop a diferentes secciones

--- newscrawler/newscrawler/spiders/diario_crawler.py
+++ newscrawler/newscrawler/spiders/diario_crawler.py
@@ -1,16 +1,20 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.http import Request
 
 
 class diarioCrawler(CrawlSpider):
     name = "diarioCrawler"
     allowe_domains = ["eldiariodechihuahua.mx/"]
-    start_urls = ["https://www.eldiariodechihuahua.mx/seccion/Local/"]
+    start_urls = ["https://www.eldiariodechihuahua.mx/"]
 
 
     rule  = (
-        Rule(LinkExtractor(allow = "local")),
+        Rule(LinkExtractor(allow = "seccion"),callback = "parse_item"),
     )
 
-
-    
+    def parse_item(self, response):
+        next_page_links = response.xpath('//xpath[to_links]').getall()
+        for link in next_page_links:
+            yield Request(response.urljoin(link), callback=self.parse_item # o otro callback
+                          )
